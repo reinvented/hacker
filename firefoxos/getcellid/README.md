bd66cbef424d2ae1dec6c2eea980061a7510028c
Peter Rukavina <peter@rukavina.net>
2013-06-16 22:29:31 -0300

    Added Get Cell ID App, first release.
    An app to get the current Cell ID and echo it to OpenCellID.org.

---
+++ firefoxos/getcellid/README.md
@@ -0,0 +1,25 @@
+Get Cell ID
+===========
+
+This is an open web app, targetted at Firefox OS and tested initially on the Geeksphone Peak, that grabs the current cellular network ID (the MCC, MNC, LAC and Cell ID) and displays them.
+
+The app will also, optionally, upload this information to the [OpenCellID.org](http://opencellid.org) project via its API.
+
+TODO
+----
+
+* Make the main screen scrollable to accommodate devices with smaller screens (the "Refresh" button gets cut off in the Firefox OS Simulator and, I presume, on the Geeksphone Keon)
+* Handle failure, especially of the OpenCellID.org update, more gracefully (or, indeed, at all), and perhaps check the OpenCellID.org key for validity when it's saved on the "Settings" page.
+* Make the Cell ID "clickable", generating a query to the OpenCellID.org API that accepts a Cell ID and returns its guess of geolocation.
+
+Uses
+----
+
+* [Zepto.js](http://zeptojs.com/)
+* [Moment.js](http://momentjs.com/)
+
+
+Screenshot
+----------
+
+![image](screenshots/getcellid-screenshot.png)
---
+++ firefoxos/getcellid/www/js/cellid.js
@@ -0,0 +1,105 @@
+var currentPosition = false;
+var currentPositionLatitude = false;
+var currentPositionLongitude = false;
+var positionInterval = false;
+var positionError = false;
+var cellids = new Object();
+var reportssent = 0;
+
+startUp();
+
+function startUp() {
+	navigator.geolocation.getCurrentPosition(successGeolocation, errorNoGeolocation, { enableHighAccuracy: true, maximumAge: 0 });
+	getCellID();
+	$("#opencellid").val(localStorage.opencellid);
+	window.setInterval(getCellID,30000);
+}
+
+$('#refresh').click(function(){
+  getCellID();
+})
+
+function getCellID() {
+  var conn = window.navigator.mozMobileConnection;
+  if (!conn || !conn.voice || !conn.voice.network) {
+    return;
+  }
+	else {
+		var gsmCellId  = conn.voice.cell.gsmCellId;
+		var gsmLocationAreaCode = conn.voice.cell.gsmLocationAreaCode;
+		var longName = conn.voice.network.longName;
+		var mcc = conn.voice.network.mcc;
+		var mnc = conn.voice.network.mnc;
+		var relSignalStrength = conn.voice.relSignalStrength;
+		var signalStrength = conn.voice.signalStrength;
+
+		$("#gsmCellId").html(gsmCellId);
+		$("#gsmLocationAreaCode").html(gsmLocationAreaCode);
+		$("#longName").html(longName);
+		$("#mcc").html(mcc);
+		$("#mnc").html(mnc);
+		$("#relSignalStrength").html(relSignalStrength + "/" + signalStrength);
+
+		if (!cellids[gsmCellId]) {
+			cellids[gsmCellId] = true;
+		}
+
+		if ((currentPositionLatitude) && (localStorage.opencellid != '')) {
+			var xhr = new XMLHttpRequest({mozSystem: true, responseType: 'json'});
+			var geturl = "http://www.opencellid.org/measure/add?key=7259c162f255e25e288c6e991fe11592&cellid=" + gsmCellId + "&lac=" + gsmLocationAreaCode + "&mcc=" + mcc + "&mnc=" + mnc + "&signal=" + relSignalStrength + "&lat=" + currentPositionLatitude + "&lon=" + currentPositionLongitude + "&measured_at=" + moment().format();
+			xhr.open('GET', geturl, true);
+			xhr.send();
+			reportssent = reportssent + 1;
+			$("#reportssent").html(reportssent);
+		}
+	}
+}
+
+function speakUpdate() {
+	var audioupdate = new Audio("audio/location.mp3"); 
+	audioupdate.play();
+}
+
+function successGeolocation(position) {
+		speakUpdate();
+    currentPosition = (Math.round(position.coords.latitude * 100) / 100) + ","  + (Math.round(position.coords.longitude * 100) / 100);
+    currentPositionLatitude = position.coords.latitude;
+    currentPositionLongitude = position.coords.longitude;
+    $("#location").html(currentPosition);
+
+    if (positionInterval) {
+        navigator.geolocation.clearWatch(positionInterval);
+    }
+    positionInterval = navigator.geolocation.watchPosition(updatePosition, noPositionFound, { enableHighAccuracy: true, maximumAge: 0, frequency: 10000 });
+    positionError = false;
+}
+
+function errorNoGeolocation(error) {
+}
+
+function updatePosition(position) {
+    currentPosition = (Math.round(position.coords.latitude * 1000) / 1000) + ","  + (Math.round(position.coords.longitude * 1000) / 1000);
+    $("#location").html(currentPosition);
+    currentPositionLatitude = position.coords.latitude;
+    currentPositionLongitude = position.coords.longitude;
+}
+
+function noPositionFound() {
+}
+
+$('#settings-btn').bind('click', function () {
+	$('#settings-view').removeClass('move-down');
+	$('#settings-view').addClass('move-up');
+});
+
+$('#close-btn').bind('click', function () {
+	$('#settings-view').removeClass('move-up');
+	$('#settings-view').addClass('move-down');
+});
+
+$('#done-btn').bind('click', function () {
+	window.localStorage.setItem("opencellid", $("#opencellid").val());
+	$('#settings-view').removeClass('move-up');
+	$('#settings-view').addClass('move-down');
+});
+
