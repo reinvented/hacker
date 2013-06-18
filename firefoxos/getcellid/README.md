6f00430919ec94936a88553efc7b6ae06664fc3e
Peter Rukavina <peter@rukavina.net>
2013-06-18 14:50:41 -0300

    Added a setting for update frequency.
    The update frequency to OpenCellID.org is now user-setting in the
    Setting page, and the cell information is automatically updated every
    second, so there was no need for a "Refresh" button, so I removed it.

--- firefoxos/getcellid/README.md
+++ firefoxos/getcellid/README.md
@@ -5,8 +5,8 @@ This is an open web app, targetted at Firefox OS and tested initially on the Gee
 
 The app will also, optionally, upload this information to the [OpenCellID.org](http://opencellid.org) project via its API.
 
-TODO
-----
+To Do
+-----
 
 * Handle failure, especially of the OpenCellID.org update, more gracefully (or, indeed, at all), and perhaps check the OpenCellID.org key for validity when it's saved on the "Settings" page.
 * Make the Cell ID "clickable", generating a query to the OpenCellID.org API that accepts a Cell ID and returns its guess of geolocation.
--- firefoxos/getcellid/www/js/cellid.js
+++ firefoxos/getcellid/www/js/cellid.js
@@ -3,26 +3,29 @@ var currentPositionLatitude = false;
 var currentPositionLongitude = false;
 var positionInterval = false;
 var positionError = false;
-var cellids = new Object();
 var reportssent = 0;
+var updatefrequency = 30000;
+var updateinterval = false;
 
 startUp();
 
 function startUp() {
 	getCellID();
+	if (localStorage.getItem("updatefrequency") === null) {
+		window.localStorage.setItem("updatefrequency", updatefrequency);
+	}
 	$("#opencellid").val(localStorage.opencellid);
+	$("#updatefrequency").val(localStorage.updatefrequency);
 	if (localStorage.send_to_opencellid == 'on') {
 		navigator.geolocation.getCurrentPosition(successGeolocation, errorNoGeolocation, { enableHighAccuracy: true, maximumAge: 0 });
 		$("#send_to_opencellid").attr('checked',true);
 		$("#gpslocation").show();
-		$("#reportssent").show();
+		$("#reportssent_wrapper").show();
+		updateinterval = window.setInterval(sendToOpenCellID,localStorage.updatefrequency);
+		console.log("Setting update frequency to " + localStorage.updatefrequency);
 	}
-	window.setInterval(getCellID,30000);
+	window.setInterval(getCellID,1000);
 }
-
-$('#refresh').click(function(){
-  getCellID();
-})
 
 function getCellID() {
   var conn = window.navigator.mozMobileConnection;
@@ -42,19 +45,6 @@ function getCellID() {
 		$("#network_identifiers").html(mcc + "/" + mnc + "/" + gsmLocationAreaCode);
 		$("#longName").html(longName);
 		$("#relSignalStrength").html(relSignalStrength + "/" + signalStrength);
-
-		if (!cellids[gsmCellId]) {
-			cellids[gsmCellId] = true;
-		}
-
-		if ((currentPositionLatitude) && (localStorage.opencellid != '') && (localStorage.send_to_opencellid == 'on')) {
-			var xhr = new XMLHttpRequest({mozSystem: true, responseType: 'json'});
-			var geturl = "http://www.opencellid.org/measure/add?key=7259c162f255e25e288c6e991fe11592&cellid=" + gsmCellId + "&lac=" + gsmLocationAreaCode + "&mcc=" + mcc + "&mnc=" + mnc + "&signal=" + relSignalStrength + "&lat=" + currentPositionLatitude + "&lon=" + currentPositionLongitude + "&measured_at=" + moment().format();
-			xhr.open('GET', geturl, true);
-			xhr.send();
-			reportssent = reportssent + 1;
-			$("#reportssent").html(reportssent);
-		}
 	}
 }
 
@@ -73,7 +63,7 @@ function successGeolocation(position) {
     if (positionInterval) {
         navigator.geolocation.clearWatch(positionInterval);
     }
-    positionInterval = navigator.geolocation.watchPosition(updatePosition, noPositionFound, { enableHighAccuracy: true, maximumAge: 0, frequency: 10000 });
+    positionInterval = navigator.geolocation.watchPosition(updatePosition, noPositionFound, { enableHighAccuracy: true, maximumAge: 0 });
     positionError = false;
 }
 
@@ -81,13 +71,40 @@ function errorNoGeolocation(error) {
 }
 
 function updatePosition(position) {
-    currentPosition = (Math.round(position.coords.latitude * 1000) / 1000) + ","  + (Math.round(position.coords.longitude * 1000) / 1000);
-    $("#location").html(currentPosition);
-    currentPositionLatitude = position.coords.latitude;
-    currentPositionLongitude = position.coords.longitude;
+	currentPosition = (Math.round(position.coords.latitude * 1000) / 1000) + ","  + (Math.round(position.coords.longitude * 1000) / 1000);
+	$("#location").html(currentPosition);
+	currentPositionLatitude = position.coords.latitude;
+	currentPositionLongitude = position.coords.longitude;
 }
 
 function noPositionFound() {
+}
+
+function sendToOpenCellID() {
+	console.log("Running sendToOpenCellID()");
+
+  var conn = window.navigator.mozMobileConnection;
+  if (!conn || !conn.voice || !conn.voice.network) {
+    return;
+  }
+	else {
+		var gsmCellId  = conn.voice.cell.gsmCellId;
+		var gsmLocationAreaCode = conn.voice.cell.gsmLocationAreaCode;
+		var longName = conn.voice.network.longName;
+		var mcc = conn.voice.network.mcc;
+		var mnc = conn.voice.network.mnc;
+		var relSignalStrength = conn.voice.relSignalStrength;
+		var signalStrength = conn.voice.signalStrength;
+
+		if ((currentPositionLatitude) && (localStorage.opencellid != '') && (localStorage.send_to_opencellid == 'on')) {
+			reportssent = reportssent + 1;
+			$("#reportssent").html(reportssent);
+			var xhr = new XMLHttpRequest({mozSystem: true, responseType: 'json'});
+			var geturl = "http://www.opencellid.org/measure/add?key=7259c162f255e25e288c6e991fe11592&cellid=" + gsmCellId + "&lac=" + gsmLocationAreaCode + "&mcc=" + mcc + "&mnc=" + mnc + "&signal=" + relSignalStrength + "&lat=" + currentPositionLatitude + "&lon=" + currentPositionLongitude + "&measured_at=" + moment().format();
+			xhr.open('GET', geturl, true);
+			xhr.send();
+		}
+	}
 }
 
 $('#settings-btn').bind('click', function () {
@@ -102,17 +119,20 @@ $('#close-btn').bind('click', function () {
 
 $('#done-btn').bind('click', function () {
 	window.localStorage.setItem("opencellid", $("#opencellid").val());
+	window.localStorage.setItem("updatefrequency", $("#updatefrequency").val());
 	if ($("#send_to_opencellid").is(':checked')) {
 		window.localStorage.setItem("send_to_opencellid", 'on');
 		navigator.geolocation.getCurrentPosition(successGeolocation, errorNoGeolocation, { enableHighAccuracy: true, maximumAge: 0 });
 		$("#gpslocation").show();
-		$("#reportssent").show();
+		$("#reportssent_wrapper").show();
+		clearInterval(updateinterval);
+		updateinterval = window.setInterval(sendToOpenCellID,localStorage.updatefrequency);
+		sendToOpenCellID();
 	} 
 	else {
 		window.localStorage.setItem("send_to_opencellid", 'off');
-		navigator.geolocation.getCurrentPosition(successGeolocation, errorNoGeolocation, { enableHighAccuracy: true, maximumAge: 0 });
 		$("#gpslocation").hide();
-		$("#reportssent").hide();
+		$("#reportssent_wrapper").hide();
 		if (positionInterval) {
 			navigator.geolocation.clearWatch(positionInterval);
 		}
